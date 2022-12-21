2a2e213c9f0f571cec85aa9e4c40b44c1739f011
Valerii-Denisov <klerk919191@gmail.com>
2022-12-21 14:45:29 +0300

    trying to deploy step 1 to railways app

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', False)
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
 ALLOWED_HOSTS = ['*', '.railway.com', '127.0.0.1']
 
