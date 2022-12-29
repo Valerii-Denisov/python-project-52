2d01128a75ab66d53ff475294cbaed22bb473f64
Valerii-Denisov <klerk919191@gmail.com>
2022-12-29 11:50:36 +0300

    connecting i18n

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -113,13 +113,18 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+# LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
+USE_L10N = True
+
 USE_TZ = True
+
+LOCALE_PATHS = (os.path.join(BASE_DIR, 'locale'), )
 
 
 # Static files (CSS, JavaScript, Images)
